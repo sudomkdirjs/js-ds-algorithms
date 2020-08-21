f11d0742473b784984248de1dd66161d35b21a8c
marees <mareesvlpt@gmail.com>
2020-08-21 18:03:18 +0530

    Arrays | Reverse Arrays

---
+++ test/arrays/reverse_array.js
@@ -0,0 +1,30 @@
+let chai = require('chai');
+
+const { assert } = chai;
+
+const reverseArray = array => {
+
+    for (let i = 0; i < array.length / 2; i++) {
+        let temp = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = temp;
+    }
+    return array;
+}
+
+const _reverseArray = array => {
+    
+    for (let i = 0; i < array.length / 2; i++) {
+        // Array Destructuring
+        [array[i], array[array.length - 1 - i]] = [array[array.length - 1 - i], array[i]];
+    }
+    return array;
+}
+
+describe("Reverse Arrays", () => {
+    it("Should reverse arrays", () => {
+        assert.deepEqual(reverseArray([1, 2, 3, 4]), [4, 3, 2, 1]);
+        assert.deepEqual(reverseArray([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1]);
+        assert.deepEqual(_reverseArray([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1]);
+    });
+});
